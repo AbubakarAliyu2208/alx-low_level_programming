c26cd212901cb1577a0ccf0b483f887b83d7d639
Abubakar Aliyu <abubakaraliyu2208@gmail.com>
2022-09-10 20:20:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
  * main - printing the string in the put function
  *
  * Describtion: using the main function
- *this program prints "programming is like building a multilingual puzzle
+ *this program prints "programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)
 {
 	puts("programming is like building a multilingual puzzle");
 	return (0);
-}
+}	
+
