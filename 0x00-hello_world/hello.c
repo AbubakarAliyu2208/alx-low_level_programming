69872398014be4f1410b8b98b89d0b8cee965a3c
Abubakar Aliyu <abubakaraliyu2208@gmail.com>
2022-09-11 07:24:08 -0700

    4-puts.c

---
+++ 0x00-hello_world/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print the string in the put function
+ *
+ * Describtion: using the main function
+ * Return: 0
+ */
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle");
+	return (0);
+}
